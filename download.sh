ce1883bf8031b04cbdfa3ee346d7ff20e4c2a120
Jinlong Feng <jinlongfeng@live.com>
2020-03-28 18:06:52 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/OP-TEE/optee_client.git
+git clone https://github.com/OP-TEE/optee_os.git
+git clone https://github.com/OP-TEE/optee_test.git
+git clone https://github.com/OP-TEE/build.git
+git clone https://github.com/linaro-swg/linux.git
+git clone https://github.com/linaro-swg/optee_benchmark.git
+git clone https://github.com/linaro-swg/optee_examples.git
+git clone https://github.com/linaro-swg/soc_term.git
+git clone https://github.com/linaro-swg/u-boot.git
+git clone https://github.com/buildroot/buildroot.git
+git clone https://git.trustedfirmware.org/trusted-firmware-a.git
+git clone https://github.com/qemu/qemu.git
+
+
